23098f2056be044715cf1baf41dfdbc133dcca76
matei_c9 <ciobanumatei61@gmail.com>
2026-07-04 01:23:32 +0300

    created tests folder

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 a=5
 b=9
 c=95
+def aq():
+    return 3
 plt.plot(xpoints, ypoints)
 plt.show()
+print(aq())
